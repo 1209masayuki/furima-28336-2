5cf051ccc19b43f2b61cb0c36c35d5f459a6978f
1209masayuki <xkiddo22@yahoo.co.jp>
2020-08-27 22:11:55 +0900

    テストコードのエラーを直しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_08_27_105834) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -50,9 +49,6 @@ ActiveRecord::Schema.define(version: 2020_08_27_105834) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
-ActiveRecord::Schema.define(version: 2020_08_16_093929) do
-
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -71,8 +67,6 @@ ActiveRecord::Schema.define(version: 2020_08_16_093929) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
-
 end
