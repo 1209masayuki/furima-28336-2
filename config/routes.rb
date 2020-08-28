f81c85d936e58240d37f8d5581a3616a7be05fff
1209masayuki <xkiddo22@yahoo.co.jp>
2020-08-28 12:52:25 +0900

    ルーティングの記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'items#index'
-  resources :items 
-  # do
-  # resources :buyers, only: [:index, :create]
-  # end
+  resources :items do
+  resources :buyers, only: [:index, :create]
+  end
   
 end
